eb724c7d0da57183a3edeeb71e4d1d40a8e7de7e
YinYanCat <chyanez2023@udec.cl>
2026-05-18 16:44:34 -0400

    [REFACTOR]: allocating the sum of matrices for strassen instead of passing a view with a pointer to that temp sum, this shouldn't affect the result in anyway and it's a bit cleaner in my point of view

--- src/strassen.cpp
+++ src/strassen.cpp
@@ -1,12 +1,12 @@
 #pragma once
 
 #include "./matrix.cpp"
-
+#include <stdexcept>
 
 Matrix Strassen(MatrixView a, MatrixView b){
     
     if (a.cols() != b.rows()) {
-        return Matrix(1,1);
+        throw std::runtime_error("Invalid operation, the given matrices can't be multiplied");
     }
 
     if (a.cols() == 1 && a.rows() == 1 && b.cols() == 1 && b.rows() == 1) {
@@ -16,78 +16,56 @@ Matrix Strassen(MatrixView a, MatrixView b){
     }
 
     Matrix c = Matrix(a.rows(), b.cols());
+
+    std::size_t ahr = a.rows()/2;                                           //a half rows
+    std::size_t ahc = a.cols()/2;                                           //a half columns
+    std::size_t bhr = b.rows()/2;                                           //b half rows
+    std::size_t bhc = b.cols()/2;                                           //b hals columns
+
     
-    Matrix M1 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2) +                             //A11
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2, a.cols()/2)       //A22
-        ).view(),
-        (
-            b.subview(b.rows()/2, b.cols()/2) +                             //B11
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2, b.cols()/2)       //B22
-        ).view()
-    );
+    Matrix temp1 = a.subview(ahr, ahc) +                                    //A11 +
+            a.subview(ahr, ahc, ahr, ahc);                                  //A22
 
-    Matrix M2 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2) +                 //A21
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2, a.cols()/2)       //A22
-        ).view(), 
-        (
-            b.subview(b.rows()/2, b.cols()/2)
-        )
-    );
+    Matrix temp2 = b.subview(bhr, bhc) +                                    //B11 +
+            b.subview(bhr, bhc, bhr, bhc);                                  //B22
 
-    Matrix M3 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2)                               //A11
-            
-        ),
-        (
-            b.subview(b.rows()/2, b.cols()/2, 0, b.cols()/2) -              //B12
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2, b.cols()/2)       //B22
-        ).view()
-    );
+    Matrix M1 = Strassen(temp1.view(), temp2.view());                       //(A11 + A22)*(B11 + B22)
 
-    Matrix M4 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2, a.cols()/2)       //A22
-        ),
-        (
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2) -                 //B21
-            b.subview(b.rows()/2, b.cols()/2)                               //B11
-        ).view()
-    );
+    temp1 = a.subview(ahr, ahc, ahr) +                                      //A21 +
+            a.subview(ahr, ahc, ahr, ahc);                                  //A22
 
-    Matrix M5 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2) +                             //A11
-            a.subview(a.rows()/2, a.cols()/2, 0, a.cols()/2)                //A12
-        ).view(),
-        (
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2, b.cols()/2)       //B22
-        )
-    );
-    Matrix M6 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2) -                 //A21
-            a.subview(a.rows()/2, a.cols()/2)                               //A11
-        ).view(),
-        (
-            b.subview(b.rows()/2, b.cols()/2) +                             //B11
-            b.subview(b.rows()/2, b.cols()/2, 0, b.cols()/2)                //B12
-        ).view()
-    );
-    Matrix M7 = Strassen(
-        (
-            a.subview(a.rows()/2, a.cols()/2, 0, a.cols()/2) -              //A12
-            a.subview(a.rows()/2, a.cols()/2, a.rows()/2, a.cols()/2)       //A22
-        ).view(),
-        (
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2) +                 //B21
-            b.subview(b.rows()/2, b.cols()/2, b.rows()/2, b.cols()/2)       //B22
-        ).view()
-    );
+    Matrix M2 = Strassen(temp1.view(), b.subview(bhr, bhc));                //(A21 + A22) * B11
+
+    temp2 = b.subview(bhr, bhc, 0, bhc) -                                   //B12 -
+            b.subview(bhr, bhc, bhr, bhc);                                  //B22
+
+    Matrix M3 = Strassen(a.subview(ahr, ahc), temp2.view());                //A11 * (B12 - B22)
+
+    temp2 = b.subview(bhr, bhc, bhr) -                                      //B21 -
+            b.subview(bhr, bhc);                                            //B11
+
+    Matrix M4 = Strassen(a.subview(ahr, ahc, ahr, ahc), temp2.view());      //A22 * (B12 - B22)
+
+    temp1 = a.subview(ahr, ahc) +                                           //A11 +
+            a.subview(ahr, ahc, 0, ahc);                                    //A12
+
+    Matrix M5 = Strassen(temp1.view(), b.subview(bhr, bhc, bhr, bhc));      //(A11 + A12) * B22
+
+    temp1 = a.subview(ahr, ahc, ahr) -                                      //A21 -
+            a.subview(ahr, ahc);                                            //A11
+
+    temp2 = b.subview(bhr, bhc) +                                           //B11 +
+            b.subview(bhr, bhc, 0, bhc);                                    //B12
+
+    Matrix M6 = Strassen(temp1.view(), temp2.view());                       //(A21 - A11) * (B11 + B12)
+
+    temp1 = a.subview(ahr, ahc, 0, ahc) -                                   //A12 -
+            a.subview(ahr, ahc, ahr, ahc);                                  //A22
+
+    temp2 = b.subview(bhr, bhc, bhr) +                                      //B21 +
+            b.subview(bhr, bhc, bhr, bhc);                                  //B22
+
+    Matrix M7 = Strassen(temp1.view(), temp2.view());                       //(A12 - A22) * (B21 + B22)
 
     MatrixView C11 = c.view(c.rows()/2, c.cols()/2);
     MatrixView C12 = c.view(c.rows()/2, c.cols()/2, 0, c.cols()/2);
