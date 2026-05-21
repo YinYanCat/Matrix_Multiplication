8cdd8936f003fd720de33d1601d125dd1c40b5fa
YinYanCat <chyanez2023@udec.cl>
2026-05-21 01:21:12 -0400

    [REFACTOR]: polishing Matrix class

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,19 +2,12 @@
 #include <cstddef>
 #include <stdexcept>
 
-class Matrix;
-
-template <typename A, typename B, typename Oprt>
-Matrix add(const A& a, const B& b, Oprt operation);
-
-template <typename A, typename B>
-Matrix Naive(const A& a, const B& b);
-
 class Matrix {
 
     public:
+    
     Matrix(std::size_t r, std::size_t c) 
-        : cols_(c), rows_(r), data_(c*r) {}
+        : cols_(c), rows_(r), data_(c*r,0) {}
     
     std::size_t cols() const { return cols_; }
     std::size_t rows() const { return rows_; }
@@ -25,14 +18,6 @@ class Matrix {
 
     double operator()(std::size_t r, std::size_t c) const {
         return data_[r * cols_ + c];
-    }
-
-    Matrix operator+(const Matrix& a) const {
-        return add((*this), a, std::plus<double>());
-    }
-
-    Matrix operator-(const Matrix& a) const {
-        return add((*this), a, std::minus<double>());
     }
 
     // TODO
@@ -68,16 +53,28 @@ class Matrix {
             return (*m)(row_offset + r, col_offset + c);
         }
 
-        Matrix operator+(const MatrixView& a) const {
-            return add(*this, a, std::plus<double>());
-        }
-        Matrix operator-(const MatrixView& a) const {
-            return add(*this, a, std::minus<double>());
+        void operator+=(const MatrixView& a) {
+            for (std::size_t i = 0; i < rows_; i++) {
+                for (std::size_t j = 0; j < cols_; j++) {
+                    (*this)(i,j) += a(i,j);
+                }
+            }
         }
 
-        void assign(const Matrix& a) {
-            for(std::size_t i = 0; i < rows_; i++) {
-                for(std::size_t j = 0; j < cols_; j++) {
+        void operator-=(const MatrixView& a) {
+            for (std::size_t i = 0; i < rows_; i++) {
+                for (std::size_t j = 0; j < cols_; j++) {
+                    (*this)(i,j) -= a(i,j);
+                }
+            }
+        }
+
+        Matrix operator+(const MatrixView& a) const { return add(*this, a, std::plus<double>()); }
+        Matrix operator-(const MatrixView& a) const { return add(*this, a, std::minus<double>()); }
+
+        void copy(const MatrixView& a) {
+            for (std::size_t i = 0; i < rows_; i++) {
+                for (std::size_t j = 0; j < cols_; j++) {
                     (*this)(i,j) = a(i,j);
                 }
             }
@@ -109,6 +106,23 @@ class Matrix {
         std::size_t row_offset;
         std::size_t col_offset;
 
+        template <typename Oprt> 
+        static Matrix add(const MatrixView& a, const MatrixView& b, Oprt operation){
+            std::size_t rows_ = a.rows();
+            std::size_t cols_ = a.cols();
+
+            if (rows_ != b.rows() || cols_ != b.cols()) {
+                throw std::runtime_error("Invalid operation, the given matrices have to be the same size.");
+            }
+            Matrix result(rows_, cols_);
+            for(std::size_t i = 0; i < rows_; i++){
+                for(std::size_t j = 0; j < cols_; j++){
+                    result(i,j) = operation(a(i,j),b(i,j));
+                }
+            }
+            return result;
+        }
+
     };
 
     MatrixView view(std::size_t rows = 0, std::size_t cols = 0, std::size_t row_offset = 0, std::size_t col_offset = 0) {
@@ -123,6 +137,22 @@ class Matrix {
                 row_offset,
                 col_offset
         );
+    }
+
+    template <typename A, typename B>
+    static Matrix Naive(const A& a, const B& b) {
+        if (a.cols() != b.rows()) {
+            throw std::runtime_error("Invalid operation, the given matrices can't be multiplied.");
+        }
+        Matrix c(a.rows(), b.cols());
+        for (std::size_t i = 0; i < a.rows(); i++) {
+            for (std::size_t j = 0; j < b.cols(); j++) {
+                for (std::size_t k = 0; k < a.cols(); k++) {
+                    c(i,j) += a(i,k) * b(k,j);
+                }
+            }
+        }
+        return c;
     }
 
     Matrix Strassen(MatrixView a, MatrixView b, std::size_t until_size){
@@ -155,102 +185,62 @@ class Matrix {
         std::size_t bhr = b.rows()/2;                                                       // b half rows
         std::size_t bhc = b.cols()/2;                                                       // b half columns
 
-        
-        Matrix temp1 = a.subview(ahr, ahc) +                                                //A11 +
-                a.subview(ahr, ahc, ahr, ahc);                                              //A22
+        MatrixView A11 = a.subview(ahr, ahc);
+        MatrixView A12 = a.subview(ahr, ahc, 0, ahc);
+        MatrixView A21 = a.subview(ahr, ahc, ahr, 0);
+        MatrixView A22 = a.subview(ahr, ahc, ahr, ahc);
 
-        Matrix temp2 = b.subview(bhr, bhc) +                                                //B11 +
-                b.subview(bhr, bhc, bhr, bhc);                                              //B22
-
-        Matrix M1 = Strassen(temp1.view(), temp2.view(), until_size);                       //(A11 + A22)*(B11 + B22)
-
-        temp1 = a.subview(ahr, ahc, ahr) +                                                  //A21 +
-                a.subview(ahr, ahc, ahr, ahc);                                              //A22
-
-        Matrix M2 = Strassen(temp1.view(), b.subview(bhr, bhc), until_size);                //(A21 + A22) * B11
-
-        temp2 = b.subview(bhr, bhc, 0, bhc) -                                               //B12 -
-                b.subview(bhr, bhc, bhr, bhc);                                              //B22
-
-        Matrix M3 = Strassen(a.subview(ahr, ahc), temp2.view(), until_size);                //A11 * (B12 - B22)
-
-        temp2 = b.subview(bhr, bhc, bhr) -                                                  //B21 -
-                b.subview(bhr, bhc);                                                        //B11
-
-        Matrix M4 = Strassen(a.subview(ahr, ahc, ahr, ahc), temp2.view(), until_size);      //A22 * (B12 - B22)
-
-        temp1 = a.subview(ahr, ahc) +                                                       //A11 +
-                a.subview(ahr, ahc, 0, ahc);                                                //A12
-
-        Matrix M5 = Strassen(temp1.view(), b.subview(bhr, bhc, bhr, bhc), until_size);      //(A11 + A12) * B22
-
-        temp1 = a.subview(ahr, ahc, ahr) -                                                  //A21 -
-                a.subview(ahr, ahc);                                                        //A11
-
-        temp2 = b.subview(bhr, bhc) +                                                       //B11 +
-                b.subview(bhr, bhc, 0, bhc);                                                //B12
-
-        Matrix M6 = Strassen(temp1.view(), temp2.view(), until_size);                       //(A21 - A11) * (B11 + B12)
-
-        temp1 = a.subview(ahr, ahc, 0, ahc) -                                               //A12 -
-                a.subview(ahr, ahc, ahr, ahc);                                              //A22
-
-        temp2 = b.subview(bhr, bhc, bhr) +                                                  //B21 +
-                b.subview(bhr, bhc, bhr, bhc);                                              //B22
-
-        Matrix M7 = Strassen(temp1.view(), temp2.view(), until_size);                       //(A12 - A22) * (B21 + B22)
+        MatrixView B11 = b.subview(bhr, bhc);
+        MatrixView B12 = b.subview(bhr, bhc, 0, bhc);
+        MatrixView B21 = b.subview(bhr, bhc, bhr, 0);
+        MatrixView B22 = b.subview(bhr, bhc, bhr, bhc);
 
         MatrixView C11 = c.view(c.rows()/2, c.cols()/2);
         MatrixView C12 = c.view(c.rows()/2, c.cols()/2, 0, c.cols()/2);
         MatrixView C21 = c.view(c.rows()/2, c.cols()/2, c.rows()/2);
         MatrixView C22 = c.view(c.rows()/2, c.cols()/2, c.rows()/2, c.cols()/2);
+        
+        Matrix temp1 = A11 + A22;
+        Matrix temp2 = B11 + B22;
+        Matrix M1 = Strassen(temp1.view(), temp2.view(), until_size);       // (A11 + A22) * (B11 + B22)
 
-        C11.assign(M1 + M4 + M7 - M5);
-        C12.assign(M3 + M5);
-        C21.assign(M2 + M4);
-        C22.assign(M1 - M2 + M3 + M6);
+        temp1 = A21 + A22;
+        Matrix M2 = Strassen(temp1.view(), B11, until_size);                // (A21 + A22) * B11
+
+        temp2 = B12 - B22;
+        Matrix M3 = Strassen(A11, temp2.view(), until_size);                // A11 * (B12 - B22)
+
+        temp2 = B21 - B11;
+        Matrix M4 = Strassen(A22, temp2.view(), until_size);                // A22 * (B21 - B11)
+
+        temp1 = A11 + A12;
+        Matrix M5 = Strassen(temp1.view(), B22, until_size);                // (A11 + A12) * B22
+
+        temp1 = A21 - A11;
+        temp2 = B11 + B12;
+        Matrix M6 = Strassen(temp1.view(), temp2.view(), until_size);       // (A21 - A11) * (B11 + B12)
+
+        temp1 = A12 - A22;
+        temp2 = B21 + B22;
+        Matrix M7 = Strassen(temp1.view(), temp2.view(), until_size);       // (A12 - A22) * (B21 + B22)
+
+        C11.copy(M1.view());
+        C11 += M4.view();
+        C11 += M7.view();
+        C11 -= M5.view();
+
+        C12.copy(M3.view());
+        C12 += M5.view();
+
+        C21.copy(M2.view());
+        C21 += M4.view();
+
+        C22.copy(M1.view());
+        C22 -= M2.view();
+        C22 += M3.view();
+        C22 += M6.view();
         
         return c;
     }
 
 };
-
-template <typename A, typename B, typename Oprt> 
-Matrix add(const A &a,const B &b, Oprt operation){
-    std::size_t rows_ = a.rows();
-    std::size_t cols_ = a.cols();
-
-    if (rows_ != b.rows() || cols_ != b.cols()) {
-        throw std::runtime_error("Invalid operation, the given matrices have to be the same size.");
-    }
-
-    Matrix result(rows_, cols_);
-
-    for(std::size_t i = 0; i < rows_; i++){
-        for(std::size_t j = 0; j < cols_; j++){
-            result(i,j) = operation(a(i,j),b(i,j));
-        }
-    }
-
-    return result;
-}
-
-template <typename A, typename B>
-Matrix Naive(const A& a, const B& b){
-    
-    if (a.cols() != b.rows()) {
-        throw std::runtime_error("Invalid operation, the given matrices can't be multiplied.");
-    }
-
-    Matrix result = Matrix(a.rows(), b.cols());
-
-    for (std::size_t i = 0; i < a.rows(); i++) {
-        for (std::size_t j = 0; j < b.cols(); j++) {
-            for (std::size_t k = 0; k < a.cols(); k++) {
-                result(i,j) += a(i,k) * b(k,j);
-            }
-        }
-    }
-        
-    return result;
-}
